09cfebd398a5d1fcd7b7e851816c3a2001135ea4
Pro Codes <Jablessions76@gmail.com>
2024-02-10 03:39:57 -0800

    edited

---
+++ css/css.php
@@ -0,0 +1,6 @@
+<link rel="stylesheet" href="../css/cdn.jsdelivr.net_npm_bootstrap@4.6.2_dist_css_bootstrap.min.css">
+<link rel="stylesheet" href="../css/style.css">
+<script src="../css/cdn.jsdelivr.net_npm_jquery@3.6.4_dist_jquery.slim.min.js"></script>
+<script src="../css/cdn.jsdelivr.net_npm_popper.js@1.16.1_dist_umd_popper.min.js"></script>
+<script src="../css/cdn.jsdelivr.net_npm_bootstrap@4.6.2_dist_js_bootstrap.bundle.min.js"></script>
+<script src="../myIcons.js"></script>
